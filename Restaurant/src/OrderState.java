60b4e04749723c5f36d74a2cabfdc1c2f0367c7e
clzeng123 <64665261+clzeng123@users.noreply.github.com>
2022-11-15 23:58:13 -0500

    Delete Restaurant/src directory

--- Restaurant/src/OrderState.java
+++
@@ -1,9 +0,0 @@
-public abstract class OrderState {
-    String state;
-    String display;
-    double total;
-    abstract public void order_place();
-    abstract public void order_paid();
-    abstract public void order_wait();
-    abstract public void order_receive();
-}
